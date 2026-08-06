a45941ac28ff582bde1b4b14a270671e526dd841
Pravin Kumar <erantalinrenola@gmail.com>
2026-08-06 22:12:31 +0530

    Create application architecture and main layout

--- frontend/creator-studio-ui/src/app/app.ts
+++ frontend/creator-studio-ui/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MainLayout } from './layouts/main/main-layout/main-layout';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [MainLayout],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ frontend/creator-studio-ui/src/app/layouts/main/main-layout/main-layout.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-layout.html',
+  styleUrl: './main-layout.scss'
+})
+export class MainLayout {
+
+}
